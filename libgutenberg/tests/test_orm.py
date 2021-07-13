8fdaacd274ea4a387d47f7991878d1a3064e56e3
eric <eric@hellman.net>
2021-07-13 16:33:51 -0400

    make test_orm do something!

--- libgutenberg/tests/test_orm.py
+++ libgutenberg/tests/test_orm.py
@@ -3,9 +3,13 @@
 
 import unittest
 
+from sqlalchemy import select
+from sqlalchemy.sql import func
+
 from libgutenberg import GutenbergDatabase
 
 from libgutenberg.CommonOptions import Options
+from libgutenberg.Models import Book
 
 options = Options()
 options.config = None
@@ -15,8 +19,13 @@ db_exists = GutenbergDatabase.db_exists
 class TestORM(unittest.TestCase):
 
     def setUp(self):
-        ob = GutenbergDatabase.Objectbase()
+        ob = GutenbergDatabase.Objectbase(False)
         self.session = ob.get_session()
+
+    def test_query(self):
+        num_books = self.session.query(Book).count()
+        mx = self.session.execute(select(func.max(Book.pk))).scalars().first()
+        print(mx)
 
     def tearDown(self):
         pass
