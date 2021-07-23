b1a61568acc6236aa8eb4397c965f5a67f0d0241
eric <eric@hellman.net>
2021-07-23 17:32:54 -0400

    Create DBUtils.py

---
+++ libgutenberg/DBUtils.py
@@ -0,0 +1,79 @@
+from sqlalchemy import not_
+from sqlalchemy import select
+from sqlalchemy.sql import func
+
+from libgutenberg import Models
+from libgutenberg import GutenbergDatabase as gdb
+from libgutenberg.Logger import info, debug, warning, error, exception
+
+
+OB = gdb.Objectbase(False)
+
+def ebook_exists(ebook):
+    session = OB.get_session()
+    ebook = int(ebook)
+    try:
+        in_db = session.query(Models.Book).where(Models.Book.pk == ebook).first()
+        
+    except Exception:
+        exception("Error checking for book.")
+        return False
+
+    if in_db:
+        return True
+    else:
+        info("No ebook #%d in database.", ebook)
+        return False
+
+def is_not_text(ebook):
+    session = OB.get_session()
+    return session.query(Models.Book).filter(Models.Book.pk == ebook).first().categories
+        
+def remove_ebook(ebook):
+    session = OB.get_session()
+    ebook = int(ebook)
+    session.query(Models.Book).where(Models.Book.pk == ebook).delete()
+    session.commit()
+
+def author_exists(author):
+    session = OB.get_session()
+    return session.query(Models.Author).where(Models.Author.author == author).first()
+    
+def filetype_books(filetype):
+    session = OB.get_session()
+    return session.execute(select(Models.File.fk_books).where(
+            not_(Models.File.archive_path.regexp_match('^cache/')),
+            Models.File.fk_filetypes == filetype ,
+        ).distinct()).scalars().all()
+
+def get_lang(language):
+    """ get language object from db from Struct or str """
+    session = OB.get_session()
+    lang_id = language if isinstance(language, str) else language.id
+    language = language if isinstance(language, str) else language.language
+    
+    lang = session.query(Models.Lang).where(Models.Lang.id == language).first()
+    if lang:
+        return lang
+    # check for the language name
+    lang = session.query(Models.Lang).where(Models.Lang.language == language).first()
+    return lang
+        
+def last_ebook():
+    session = OB.get_session()
+    last = session.execute(select(func.max(Models.Book.pk))).scalars().first()
+    debug("Last ebook: #%d" % last)
+    return last
+
+def recent_books(interval):
+    session = OB.get_session()
+    return session.execute(select(Models.File.fk_books).where(
+            not_(Models.File.archive_path.regexp_match('^cache/')),
+            Models.File.modified >= interval,
+        ).distinct()).scalars().all()
+
+def top_books():
+    session = OB.get_session()
+    return session.execute(select(Models.Book.pk).order_by(
+            Models.Book.downloads.desc()).limit(options.top)).scalars().all()
+            
