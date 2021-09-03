466ca0df73c8bf22f21dca92b88560804e788271
eric <eric@hellman.net>
2021-09-03 10:01:17 -0400

    skip db tests when psycopg2 not present

--- libgutenberg/tests/test_files.py
+++ libgutenberg/tests/test_files.py
@@ -3,10 +3,15 @@
 
 import unittest
 
+from libgutenberg import GutenbergDatabase
 from libgutenberg import GutenbergFiles
+
+global db_exists
+db_exists = GutenbergDatabase.db_exists
 
 class TestGutenbergFiles(unittest.TestCase):
 
+    @unittest.skipIf(not db_exists, 'database not configured')
     def test_guess_filetype(self):
         ft, enc = GutenbergFiles.guess_filetype("99999-0.txt")
         self.assertEqual(ft, 'txt')
