6cf39c369eb76ba941fdf0791a54ed3eb5974d66
eric <eric@hellman.net>
2023-02-15 09:52:03 -0500

    0.10.16

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 # libgutenberg setup.py
 #
 
-__version__ = '0.10.15'
+__version__ = '0.10.16'
 
 from setuptools import setup
 
@@ -46,9 +46,9 @@ setup (
         "Operating System :: OS Independent",
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
 
